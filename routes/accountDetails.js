3348aa5dacea2f444e5167769c704d655f64be59
Anamitra Sarkar <anamitra.sarkar@anamitrasa-ltm2.internal.salesforce.com>
2021-01-10 15:27:13 -0500

    Updated swagger doc and accounts router

--- routes/accountDetails.js
+++ routes/accountDetails.js
@@ -19,5 +19,23 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+router.get('/accounts/:account_id', async (req, res, next) => {
+
+    try{
+        const sql = 'SELECT id, account_id, account_name, tasks, zaps, plan from account_details WHERE account_id = $1';
+        const {account_id} = req.params;
+        await client
+            .query(sql, [account_id])
+            .then(result => {
+                console.log(new Date(), JSON.stringify(result));
+                res.json(result.rows);
+            })
+            .catch(err => console.error('Error executing query', err.stack));
+        console.log(new Date(), req.params);
+
+    }catch(err){
+        console.error(err.message);
+    }
+})
 
 module.exports = router;
